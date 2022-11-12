3a8b5352f268451fa4d3f974c1555c4b97b2f2f3
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-11-12 13:05:18 +0200

    Solved

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -13,26 +13,39 @@ import java.util.Arrays;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        /*StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            while (reader.ready()) {
+                result.append(reader.readLine().replace(": ", " ")).append(" ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       */
         StringBuilder result = new StringBuilder();
         try(RandomAccessFile raf = new RandomAccessFile(file, "r"); FileChannel fileChannel = raf.getChannel()){
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
             while (fileChannel.read(byteBuffer) > 0){
                 byteBuffer.flip();
                 for(int i = 0 ; i < byteBuffer.limit(); i++){
-                    result.append((char) byteBuffer.get());
+                    char symbol = (char) byteBuffer.get();
+                    if(symbol == '\r') {
+                        result.append(" ");
+                    } else if(symbol != ':') {
+                        result.append(symbol);
+                    }
                 }
                 byteBuffer.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String[] split = result.toString().split("[ :\\n\\r]");
-        System.out.println(Arrays.toString(split));
-        return new Profile(split[2], Integer.parseInt(split[6]), split[10], Long.parseLong(split[14]));
-    }
-
-    public static void main(String[] args) {
-        FileReader reader = new FileReader();
-        System.out.println(reader.getDataFromFile(new File("D:/MJCschool/stage1-module7-nio-task1/src/main/resources/Profile.txt")));
+        String[] split = result.toString().split(" ");
+        Profile profile = new Profile();
+        profile.setName(split[1]);
+        profile.setAge(Integer.parseInt(split[3]));
+        profile.setEmail(split[5]);
+        profile.setPhone(Long.parseLong(split[7]));
+        return profile;
     }
 }
