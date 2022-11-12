ab9baa9d1c11b017868d98a0a8c7c0858ff73d7b
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-11-12 11:47:05 +0200

    Solved

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder result = new StringBuilder();
+        try(RandomAccessFile raf = new RandomAccessFile(file, "r"); FileChannel fileChannel = raf.getChannel()){
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            while (fileChannel.read(byteBuffer) > 0){
+                byteBuffer.flip();
+                for(int i = 0 ; i < byteBuffer.limit(); i++){
+                    result.append((char) byteBuffer.get());
+                }
+                byteBuffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] split = result.toString().split("[ :\\n\\r]");
+        System.out.println(Arrays.toString(split));
+        return new Profile(split[2], Integer.parseInt(split[6]), split[10], Long.parseLong(split[14]));
+    }
+
+    public static void main(String[] args) {
+        FileReader reader = new FileReader();
+        System.out.println(reader.getDataFromFile(new File("D:/MJCschool/stage1-module7-nio-task1/src/main/resources/Profile.txt")));
     }
 }
