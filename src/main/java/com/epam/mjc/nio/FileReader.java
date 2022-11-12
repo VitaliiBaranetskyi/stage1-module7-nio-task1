a85a1944432a4c932bf4d2bf1198e611192fc140
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-11-12 13:14:39 +0200

    Solved

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,27 +1,15 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.Arrays;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        /*StringBuilder result = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            while (reader.ready()) {
-                result.append(reader.readLine().replace(": ", " ")).append(" ");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-       */
         StringBuilder result = new StringBuilder();
         try(RandomAccessFile raf = new RandomAccessFile(file, "r"); FileChannel fileChannel = raf.getChannel()){
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
@@ -41,11 +29,6 @@ public class FileReader {
             e.printStackTrace();
         }
         String[] split = result.toString().split(" ");
-        Profile profile = new Profile();
-        profile.setName(split[1]);
-        profile.setAge(Integer.parseInt(split[3]));
-        profile.setEmail(split[5]);
-        profile.setPhone(Long.parseLong(split[7]));
-        return profile;
+        return new Profile(split[1], Integer.parseInt(split[3]), split[5], Long.parseLong(split[7]));
     }
 }
